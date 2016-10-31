b9b2d4d3bab24f5a2d5908bd9f074755e1fa27a8
satoshi-yazawa-nii <satoshi-yazawa@nii.ac.jp>
2016-10-31 03:28:31 +0000

    Sync client.py with waterbutler

--- website/addons/weko/client.py
+++ website/addons/weko/client.py
@@ -1,18 +1,75 @@
-import httplib as http
 import logging
 import requests
-from StringIO import StringIO
-
+from io import BytesIO
 from lxml import etree
-
-from framework.exceptions import HTTPError
-
-from website.addons.weko import settings
+import base64
 
 logger = logging.getLogger('addons.weko.client')
 
 APP_NAMESPACE = 'http://www.w3.org/2007/app'
 ATOM_NAMESPACE = 'http://www.w3.org/2005/Atom'
+RDF_NAMESPACE = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
+DC_NAMESPACE = 'http://purl.org/metadata/dublin_core#'
+
+class Index(object):
+    raw = None
+    parentIdentifier = None
+
+    def __init__(self, desc):
+        self.raw = {'title': desc.find('{%s}title' % DC_NAMESPACE).text,
+                    'id': desc.find('{%s}identifier' % DC_NAMESPACE).text,
+                    'about': desc.attrib['{%s}about' % RDF_NAMESPACE]}
+
+    @property
+    def nested(self):
+        title = self.raw['title']
+        nested = 0
+        while title.startswith('--'):
+            nested += 1
+            title = title[2:]
+        return nested
+
+    @property
+    def title(self):
+        return self.raw['title'][self.nested * 2:]
+
+    @property
+    def identifier(self):
+        return self.raw['id']
+
+    @property
+    def about(self):
+        return self.raw['about']
+
+class Item(object):
+    raw = None
+    parentIdentifier = None
+
+    def __init__(self, entry):
+        self.raw = {'id': entry.find('{%s}id' % ATOM_NAMESPACE).text.strip(),
+                    'title': entry.find('{%s}title' % ATOM_NAMESPACE).text,
+                    'updated': entry.find('{%s}updated' % ATOM_NAMESPACE).text}
+
+    @property
+    def about(self):
+        return self.raw['id']
+
+    @property
+    def file_id(self):
+        return base64.b64encode(self.raw['id'].encode(encoding='utf-8')).decode(encoding='utf-8')
+
+    @property
+    def title(self):
+        return self.raw['title']
+
+    @property
+    def author(self):
+        return self.raw['author']
+
+    @property
+    def updated(self):
+        return self.raw['updated']
+
 
 class Connection(object):
     host = None
@@ -34,9 +91,29 @@ class Connection(object):
         resp = requests.get(self.host + path, headers=headers)
         if resp.status_code != 200:
             resp.raise_for_status()
-        tree = etree.parse(StringIO(resp.content))
+        tree = etree.parse(BytesIO(resp.content))
         logger.info('Connected: {}'.format(etree.tostring(tree)))
         return tree
+
+    def get_url(self, url):
+        headers = {"Authorization":"Bearer " + self.token}
+        resp = requests.get(url, headers=headers)
+        if resp.status_code != 200:
+            resp.raise_for_status()
+        tree = etree.parse(BytesIO(resp.content))
+        logger.info('Connected: {}'.format(etree.tostring(tree)))
+        return tree
+
+    def post_url(self, url, stream, headers={}):
+        headers = headers.copy()
+        headers["Authorization"] = "Bearer " + self.token
+        resp = requests.post(url, headers=headers, data=stream)
+        if resp.status_code != 200:
+            resp.raise_for_status()
+        tree = etree.parse(BytesIO(resp.content))
+        logger.info('Connected: {}'.format(etree.tostring(tree)))
+        return tree
+
 
 def _connect(host, token):
     try:
@@ -78,9 +155,45 @@ def connect_from_settings_or_401(node_settings):
     return connect_or_error(host, token)
 
 
-def get_files(dataset, published=False):
-    version = 'latest-published' if published else 'latest'
-    return dataset.get_files(version)
+def get_all_indices(connection, dataset):
+    root = connection.get('servicedocument.php')
+    indices = []
+    for desc in root.findall('.//{%s}Description' % RDF_NAMESPACE):
+        indices.append(Index(desc))
+
+    ids = []
+    for index in indices:
+        if index.nested > 0:
+            index.parentIdentifier = ids[index.nested - 1]
+
+        if len(ids) == index.nested + 1:
+            ids[index.nested] = index.identifier
+        elif len(ids) > index.nested + 1:
+            ids = ids[0:index.nested + 1]
+            ids[index.nested] = index.identifier
+        else:
+            ids.append(index.identifier)
+    return indices
+
+
+def get_items(connection, index):
+    root = connection.get_url(index.about)
+    items = []
+    for entry in root.findall('.//atom.entry'):
+        logger.info('Name: {}'.format(entry.find('{%s}title' % ATOM_NAMESPACE).text))
+        items.append(Item(entry))
+    return items
+
+
+def post(connection, target, stream, stream_size):
+    logger.info('Post: {}'.format(target))
+    weko_headers = {
+        "Content-Disposition": "filename=temp.zip",
+        "Content-Type": "application/zip",
+        "Packaging": "http://purl.org/net/sword/package/SimpleZip",
+        "Content-Length": str(stream_size),
+    }
+    return connection.post_url(target, stream, headers=weko_headers)
 
 
 def get_datasets(weko):
