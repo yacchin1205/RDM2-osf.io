4c66ddf938b690ab525b4608f7cf44421bfaa256
satoshi-yazawa-nii <satoshi-yazawa@nii.ac.jp>
2016-11-05 08:53:08 +0000

    Add file object for WEKO

--- website/files/models/__init__.py
+++ website/files/models/__init__.py
@@ -7,6 +7,7 @@ from website.files.models.github import *  # noqa
 from website.files.models.dropbox import *  # noqa
 from website.files.models.figshare import *  # noqa
 from website.files.models.dataverse import *  # noqa
+from website.files.models.weko import *  # noqa
 from website.files.models.osfstorage import *  # noqa
 from website.files.models.owncloud import *  # noqa
 from website.files.models.googledrive import *  # noqa
