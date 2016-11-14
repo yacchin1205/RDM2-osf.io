0bce960e8fed53da6241198c9719378a5942ed94
satoshi-yazawa-nii <satoshi-yazawa@nii.ac.jp>
2016-11-14 05:09:54 +0000

    Fix default config

--- website/addons/weko/settings/defaults.py
+++ website/addons/weko/settings/defaults.py
@@ -1,8 +1,8 @@
 
 REQUEST_TIMEOUT = 15
 
-REPOSITORIES = {'test.repo.nii.ac.jp':
-                 {'host': 'http://test.repo.nii.ac.jp/weko/sword/',
+REPOSITORIES = {'no_host.repo.nii.ac.jp':
+                 {'host': 'http://no_host.repo.nii.ac.jp/weko/sword/',
                   'client_id': None, 'client_secret': None,
                   'authorize_url': None,
                   'access_token_url': None}}
