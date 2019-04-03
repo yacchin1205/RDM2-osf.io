20ac2fe242f4e2613343f0965c4d4ace048adc26
shun.nakazawa <shun.nakazawa@voice-research.com>
2019-04-04 06:00:28 +0900

    Add IQB-RIMS addon base

--- api/base/settings/defaults.py
+++ api/base/settings/defaults.py
@@ -116,6 +116,7 @@ INSTALLED_APPS = (
     'addons.azureblobstorage',
     'addons.weko',
     'addons.jupyterhub',
+    'addons.iqbrims',
 )
 
 # local development using https
@@ -274,7 +275,7 @@ ENABLE_ESI = osf_settings.ENABLE_ESI
 VARNISH_SERVERS = osf_settings.VARNISH_SERVERS
 ESI_MEDIA_TYPES = osf_settings.ESI_MEDIA_TYPES
 
-ADDONS_FOLDER_CONFIGURABLE = ['box', 'dropbox', 's3', 'googledrive', 'figshare', 'owncloud', 'onedrive', 'swift', 'azureblobstorage', 'weko']
+ADDONS_FOLDER_CONFIGURABLE = ['box', 'dropbox', 's3', 'googledrive', 'figshare', 'owncloud', 'onedrive', 'swift', 'azureblobstorage', 'weko', 'iqbrims']
 ADDONS_OAUTH = ADDONS_FOLDER_CONFIGURABLE + ['dataverse', 'github', 'bitbucket', 'gitlab', 'mendeley', 'zotero', 'forward']
 
 BYPASS_THROTTLE_TOKEN = 'test-token'
