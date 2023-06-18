a3d6ea7602953d7373d86f115a194c7442dfa092
yacchin1205 <968739+yacchin1205@users.noreply.github.com>
2023-06-19 08:07:35 +0900

    Fix migrations

--- osf/migrations/0228_merge_20230609_1243.py
+++ osf/migrations/0228_merge_20230609_1243.py
@@ -8,7 +8,6 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('osf', '0227_ensure_schema_and_reports'),
         ('osf', '0226_ensure_schema_and_reports'),
     ]
 
