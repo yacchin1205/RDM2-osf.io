267328517cc78de504c1ac39e05044b0a4e25e09
yacchin1205 <968739+yacchin1205@users.noreply.github.com>
2024-01-30 15:21:44 +0900

    Merge migrations

---
+++ osf/migrations/0236_merge_20240130.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('osf', '0235_auto_20230830_0425'),
+        ('osf', '0235_merge_and_ensure_schema_and_reports'),
+    ]
+
+    operations = [
+    ]
