fceaf068f29228cb16425335faecfb6995e3e22b
yacchin1205 <968739+yacchin1205@users.noreply.github.com>
2024-06-04 07:51:37 +0900

    Improve weko and metadata UI

--- addons/metadata/static/files.js
+++ addons/metadata/static/files.js
@@ -1538,6 +1538,7 @@ function MetadataButtons() {
       const projectMetadata = context.projectMetadata;
       if (!metadata && filepath.length > 0 && filepath[filepath.length - 1] !== '/') {
         // file with no metadata
+        indicator.empty();
         return false;
       }
       const childMetadata = projectMetadata.files.filter(function(f) {
@@ -1690,7 +1691,7 @@ function MetadataButtons() {
                       m.component(base, {treebeard : tb, mode : mode,
                                   item : item }),
                     ].concat(buttons);
-                    if (item.kind === 'folder') {
+                    if (item.kind === 'folder' && !item.data.addonFullname) {
                       const importDatasetButton = new ImportDatasetButton(tb, item, {
                         assign: function() {
                           return tempIdCounterForDataset ++;
