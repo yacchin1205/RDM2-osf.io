044c4e9037ad6b6d08069d8a4cbc6670796349f4
yacchin1205 <968739+yacchin1205@users.noreply.github.com>
2024-10-28 07:31:30 +0900

    Fix max_file_size of WEKO addon

--- addons/weko/apps.py
+++ addons/weko/apps.py
@@ -1,5 +1,6 @@
 import os
 from addons.base.apps import BaseAddonAppConfig, generic_root_folder
+from addons.weko.settings import MAX_UPLOAD_SIZE
 
 weko_root_folder = generic_root_folder('weko')
 
@@ -24,7 +25,7 @@ class WEKOAddonAppConfig(BaseAddonAppConfig):
     configs = ['accounts', 'node']
     categories = ['storage']
     has_hgrid_files = True
-    max_file_size = 128  # MB
+    max_file_size = MAX_UPLOAD_SIZE
     node_settings_template = os.path.join(TEMPLATE_PATH, 'weko_node_settings.mako')
     user_settings_template = os.path.join(TEMPLATE_PATH, 'weko_user_settings.mako')
 
